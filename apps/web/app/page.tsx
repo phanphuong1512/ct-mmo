ad4b4c7fe374ece6f2c9d0f37de088bc17e3648e
KrisZ183 <155935783+hieu172149@users.noreply.github.com>
2025-12-12 21:01:09 +0700

    add image

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -22,25 +22,25 @@ const ThemeImage = (props: Props) => {
 
 export default function Home() {
  const products = [
-  { id: 1, name: "GPT Business 1 tháng", price: "50.000đ", img: "/chatGPTGo.jpg", status: "Còn hàng" },
+  { id: 1, name: "GPT Business 1 tháng", price: "50.000đ", img: "/chatgpt.webp", status: "Còn hàng" },
   { id: 2, name: "GPT Go 3 tháng", price: "60.000đ", img: "/chatGPTGo.jpg", status: "Còn hàng" },
   { id: 3, name: "GPT Go 1 năm", price: "120.000đ", img: "/chatGPTGo.jpg", status: "Còn hàng" },
   { id: 4, name: "Express VPN 1 tháng", price: "20.000đ", img: "/expressVPN.png", status: "Còn hàng" },
   { id: 5, name: "Canva 1 tháng (tài khoản cấp)", price: "10.000đ", img: "/canva.png", status: "Còn hàng" },
   { id: 6, name: "Canva 1 năm", price: "90.000đ", img: "/canva.png", status: "Tạm thời không có hàng" },
   { id: 7, name: "Perplexity 1 năm (chính chủ)", price: "250.000đ", img: "/perplexity.png", status: "Còn hàng" },
-  { id: 8, name: "Ariva Security & VPN 3 tháng", price: "60.000đ", img: "/shine/default.png", status: "Còn hàng" },
+  { id: 8, name: "Ariva Security & VPN 3 tháng", price: "60.000đ", img: "/avira.jpg", status: "Còn hàng" },
   { id: 9, name: "Spotify", price: "—", img: "/spotify_premium.jpg", status: "Tạm thời không có hàng" },
-  { id: 10, name: "Youtube", price: "—", img: "/shine/youtube.png", status: "Tạm thời không có hàng" },
-  { id: 11, name: "Figma Edu 1 năm", price: "90.000đ", img: "/shine/figma.png", status: "Tạm thời không có hàng" },
-  { id: 12, name: "Intellij 1 năm Edu", price: "90.000đ", img: "/shine/default.png", status: "Tạm thời không có hàng" },
-  { id: 13, name: "Quizlet 1 tháng", price: "9.000đ", img: "/shine/default.png", status: "Còn hàng" },
-  { id: 14, name: "Cursor trial 7 ngày", price: "20.000đ", img: "/shine/default.png", status: "Tạm thời không có hàng" },
-  { id: 15, name: "Google AI Pro 1 tháng (AI + 2TB Drive)", price: "20.000đ", img: "/shine/default.png", status: "Còn hàng" },
-  { id: 16, name: "Google AI Pro 1 năm (AI + 2TB Drive)", price: "160.000đ", img: "/shine/default.png", status: "Còn hàng" },
-  { id: 17, name: "Google AI Pro 6 tháng (AI + 2TB Drive)", price: "80.000đ", img: "/shine/default.png", status: "Còn hàng" },
-  { id: 18, name: "Dịch vụ thiết kế Website trọn gói", price: "Thương lượng", img: "/shine/default.png", status: "Cần mua lại" },
-  { id: 19, name: "Dịch vụ làm ứng dụng trọn gói", price: "Thương lượng", img: "/shine/default.png", status: "Cần mua lại" },
+  { id: 10, name: "Youtube", price: "—", img: "/ytpremium.webp", status: "Tạm thời không có hàng" },
+  { id: 11, name: "Figma Edu 1 năm", price: "90.000đ", img: "/figma.png", status: "Tạm thời không có hàng" },
+  { id: 12, name: "Intellij 1 năm Edu", price: "90.000đ", img: "/intelliJ.jpg", status: "Tạm thời không có hàng" },
+  { id: 13, name: "Quizlet 1 tháng", price: "9.000đ", img: "/quizlet.jpg", status: "Còn hàng" },
+  { id: 14, name: "Cursor trial 7 ngày", price: "20.000đ", img: "/cursor.png", status: "Tạm thời không có hàng" },
+  { id: 15, name: "Google AI Pro 1 tháng (AI + 2TB Drive)", price: "20.000đ", img: "/ggAIPro.png", status: "Còn hàng" },
+  { id: 16, name: "Google AI Pro 1 năm (AI + 2TB Drive)", price: "160.000đ", img: "/ggAIPro.png", status: "Còn hàng" },
+  { id: 17, name: "Google AI Pro 6 tháng (AI + 2TB Drive)", price: "80.000đ", img: "/ggAIPro.png", status: "Còn hàng" },
+  //{ id: 18, name: "Dịch vụ thiết kế Website trọn gói", price: "Thương lượng", img: "/shine/default.png", status: "Cần mua lại" },
+ // { id: 19, name: "Dịch vụ làm ứng dụng trọn gói", price: "Thương lượng", img: "/shine/default.png", status: "Cần mua lại" },
 ];
 
 
